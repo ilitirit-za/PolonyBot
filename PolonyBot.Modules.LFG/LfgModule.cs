64d5298c9a64d16d2c1c4da998426f8b60ebe609
sjakoet <sjakoet@sportingindex.com>
2017-09-20 18:03:08 +0200

    Solution Upgrade
    Add MvCI

--- PolonyBot.Modules.LFG/LfgModule.cs
+++ PolonyBot.Modules.LFG/LfgModule.cs
@@ -10,38 +10,39 @@ namespace PolonyBot.Modules.LFG
     // Create a module with no prefix
     public class LfgModule : ModuleBase
     {
-        private readonly Dictionary<string, string> _games = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
-        {
-            { "2K2", "King Of Fighters 2002 on Fightcade" },
-            { "3S", "Street Fighter 3: 3rd Strike on Fightcade" },
-            { "98", "King Of Fighters 98 on Fightcade" },
-            { "A2", "Street Fighter Alpha 2 on Fightcade" },
-            { "A3", "Street Fighter Alpha 3 on Fightcade" },
-            { "BB-PC", "Blazblue on PC" },
-            { "BB-PS4", "Blazblue on PS4" },
-            { "BB-X", "Blazblue on Xbox One" },
-            { "FC", "Any game on Fightcade" },
-            { "I2-PS4", "Injustice 2 on PS4" },
-            { "I2-X", "Injustice 2 on Xbox One" },
-            { "KOF13", "King of Fighters 13 on PC" },
-            { "KOF14-PC", "King Of Fighters 14 on PC" },
-            { "KOF14-PS4", "King Of Fighters 14 on PS4" },
-            { "LB2", "Last Blade 2 on Fightcade" },
-            { "MKX-PC", "Mortal Kombat X on PC" },
-            { "MKX-PS4", "Mortal Kombat X on PS4" },
-            { "MKX-X", "Mortal Kombat X on Xbox One" },
-            { "SFV", "Street Fighter V" },
-            { "SG", "Skullgirls on PC" },
-            { "ST", "Super Street Fighter 2 Turbo on Fightcade" },
-            { "SVC", "SNK vs Capcom Chaos on Fightcade" },
-            { "T7-PC", "Tekken 7 on PC" },
-            { "T7-PS4", "Tekken 7 on PS4" },
-            { "T7-X", "Tekken 7 on Xbox One" },
-            { "SF4-PC", "Ultra Street Fighter IV on PC" },
-            { "SF4-PS4", "Ultra Street Fighter IV on PS4" },
-            { "XRD-PC", "Guilty Gear Xrd on PC" },
-            { "XRD-PS4", "Guilty Gear Xrd on PS4" },
-        };
+        private readonly Dictionary<string, string> _games = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        //{
+        //    { "2K2", "King Of Fighters 2002 on Fightcade" },
+        //    { "3S", "Street Fighter 3: 3rd Strike on Fightcade" },
+        //    { "98", "King Of Fighters 98 on Fightcade" },
+        //    { "A2", "Street Fighter Alpha 2 on Fightcade" },
+        //    { "A3", "Street Fighter Alpha 3 on Fightcade" },
+        //    { "BB-PC", "Blazblue on PC" },
+        //    { "BB-PS4", "Blazblue on PS4" },
+        //    { "BB-X", "Blazblue on Xbox One" },
+        //    { "FC", "Any game on Fightcade" },
+        //    { "I2-PS4", "Injustice 2 on PS4" },
+        //    { "I2-X", "Injustice 2 on Xbox One" },
+        //    { "KOF13", "King of Fighters 13 on PC" },
+        //    { "KOF14-PC", "King Of Fighters 14 on PC" },
+        //    { "KOF14-PS4", "King Of Fighters 14 on PS4" },
+        //    { "LB2", "Last Blade 2 on Fightcade" },
+        //    { "MKX-PC", "Mortal Kombat X on PC" },
+        //    { "MKX-PS4", "Mortal Kombat X on PS4" },
+        //    { "MKX-X", "Mortal Kombat X on Xbox One" },
+        //    { "SFV", "Street Fighter V" },
+        //    { "SG", "Skullgirls on PC" },
+        //    { "ST", "Super Street Fighter 2 Turbo on Fightcade" },
+        //    { "SVC", "SNK vs Capcom Chaos on Fightcade" },
+        //    { "T7-PC", "Tekken 7 on PC" },
+        //    { "T7-PS4", "Tekken 7 on PS4" },
+        //    { "T7-X", "Tekken 7 on Xbox One" },
+        //    { "SF4-PC", "Ultra Street Fighter IV on PC" },
+        //    { "SF4-PS4", "Ultra Street Fighter IV on PS4" },
+        //    { "XRD-PC", "Guilty Gear Xrd on PC" },
+        //    { "XRD-PS4", "Guilty Gear Xrd on PS4" },
+        //};
 
         private static readonly List<LfgEntry> LfgList = new List<LfgEntry>();
 
@@ -49,6 +50,8 @@ namespace PolonyBot.Modules.LFG
         public async Task Lfg(string game = null, string command = null)
         {
             var response = "";
+
+            await LoadGameList();
 
             LfgList.RemoveAll(x => x.Expiry < DateTime.Now);
 
@@ -77,6 +80,26 @@ namespace PolonyBot.Modules.LFG
                 response = RegisterPlayer(Context.User, game, (command ?? "").Trim());
                 await ReplyAsync(response);
             }
+        }
+
+        private async Task LoadGameList()
+        {
+            _games.Clear();
+            try
+            {
+                var lines = System.IO.File.ReadAllLines("games.txt");
+                foreach (var line in lines)
+                {
+                    var split = line.Split('|');
+
+                    _games.Add(split[0], split[1]);
+                }
+            }
+            catch (Exception)
+            {
+                await ReplyAsync("Could not load game list.  Tell ilitirit about this!");
+            }
+            
         }
 
         private string GetHelpMessage()
