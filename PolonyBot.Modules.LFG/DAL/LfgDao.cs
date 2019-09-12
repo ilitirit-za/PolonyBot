597b9ce8aa001901c9688951eca0b54a034a9770
Salie Jakoet <sjakoet@sportingindex.com>
2019-09-12 15:34:03 +0200

    Cleanup unused imports

--- PolonyBot.Modules.LFG/DAL/LfgDao.cs
+++ PolonyBot.Modules.LFG/DAL/LfgDao.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+﻿using System.Data;
 using System.Data.Common;
 using System.Data.SQLite;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace PolonyBot.Modules.LFG.DAL
